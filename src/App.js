1d3e6d8848b2d860b9efad7b60552bf020a652fd
Githaka <githakamaru@gmail.com>
2020-04-11 01:46:24 +0300

    made successful api calls to covid19.mathdro.id/api, rendered the data on my UI and added some material UI components

--- src/App.js
+++ src/App.js
@@ -11,22 +11,28 @@ import styles from './App.module.css'
 import { fetchData } from './api';
 
 class App extends React.Component {
+    state = {
+        data: {},
+    }
 
     async componentDidMount() {
-        const data = await fetchData();
+        const fetchedData = await fetchData();
 
-        console.log(data);
+        this.setState({ data: fetchedData });
+
+        //console.log(data);
     }
 
     render() {
+        const { data } = this.state
         return (
             <div className={styles.container}>
-                <h1> It's working</h1>
-                <Cards />
+
+                <Cards data={data} />
                 <CountryPicker />
                 <Chart />
             </div>
-        )
+        );
     }
 }
 
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
-const Cards = () => {
+import styles from './Cards.module.css';
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if (!confirmed) {
+        return 'Loading.....';
+    }
+    //console.log(props);
+
     return (
-        <h1> Cards</h1>
-    )
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography varaint="h5">
+                            < CountUp
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography varaint="body2"> Number of active cases of Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography varaint="h5">
+                            < CountUp
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2"> Number of people who have recovered from corona</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography varaint="h5">
+                            < CountUp
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2"> Number of deaths caused by Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+
+        </div>
+    );
 }
 
 export default Cards
